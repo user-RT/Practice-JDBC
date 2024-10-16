06c3a1f669dc495ce8593a6e071cdb4c2b5bf325
khoi <timsinaravi23@gmail.com>
2024-10-16 16:35:57 +0545

    first commit

---
+++ src/DatabaseConnection.java
@@ -0,0 +1,27 @@
+import org.postgresql.DriverManager;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+        class Main {
+    public Connection connnecttodb(String dbname, String user, String pass) {
+        Connection conn = null;
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/" + dbname, user, pass);
+            if (conn != null) {
+                System.out.println("Connection Established");
+            } else {
+                System.out.println("Connection Failed");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return conn;
+    }
+
+        }
+
